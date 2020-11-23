8dccd833ecac421c91978afc1c541df1713f3885
nisholin <61735805+nisholin@users.noreply.github.com>
2020-11-23 18:05:54 +0530

    new push

--- index.js
+++ index.js
@@ -14,100 +14,126 @@ app.use(bodyParser.json());
 
 
  app.get('/api/users',(request, response)=>{
-    pool.query('SELECT * FROM employee_master',(err,result)=>{
-        if(err) throw err;
-        response.send(result);
-    });
-}); 
+        pool.query('SELECT * FROM employee_master',(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    }); 
 
-app.put("/api/users/update/:emp_code", (req , res)=>{
-    var postData  = req.body;
-    console.log(postData);
-    pool.query('UPDATE employee_master SET ? WHERE emp_code',postData,(err,result)=>{
-        if(err) throw err;
-        console.log("row updated");
-        response.send(result);
+    app.put("/api/users/update/:emp_code", (req , res)=>{
+        var postData  = req.body;
+        console.log(postData);
+        pool.query('UPDATE employee_master SET ? WHERE emp_code',postData,(err,result)=>{
+            if(err) throw err;
+            console.log("row updated");
+            response.send(result);
+        });
     });
-});
 
 
 
 
 //Manual Entry
-//Employee crud operation
-app.get('/manualentry/employee',(request, response)=>{
-    pool.query('SELECT me.id,e.emp_code,me.item_count,me.code,me.id, me.canteen_type, me.item_id,me.category_id,c.category_name, e.emp_name,me.date,me.status FROM `manual_entry` me JOIN employee_master e ON e.emp_code = me.emp_code    JOIN category_master c ON c.id = me.category_id',(err,result)=>{
-        if(err) throw err;
-        response.send(result);
-    });
-});
-app.get('/manualentry/manualentryemp/:empcode',(request, response)=>{
-    var emp_code = request.params.empcode;
-    //console.log(emp_code);
-    pool.query("SELECT em.emp_name, em.category_id,cm.category_name FROM employee_master as em INNER JOIN category_master cm ON em.category_id=cm.id where em.emp_code= ?",emp_code,(err,result)=>{
-        if(err) throw err;
-        response.send(result);
-    });
-});
-app.get('/manualentry/employee/item',(request, response)=>{
-    var postData = request.body
-    pool.query("select item_id,item_name from item_master",postData,(err,result)=>{
-        if(err) throw err;
-        response.send(result);
-    });
-});
-app.get('/manualentry/employee/category',(request, response)=>{
-    var postData = request.body
-    pool.query("select id,category_name from category_master",postData,(err,result)=>{
-        if(err) throw err;
-        response.send(result);
-    });
-});
-app.post('/manualentry/manualentrynew',(request, response)=>{
-    var  empEntryDtata  = request.body;
-    var empEntyLength   = empEntryDtata.length;
-    for(i=0;i<empEntyLength;i++) {
-        pool.query("insert into  manual_entry SET ?",empEntryDtata[i],(err,result)=>{
+    //Employee crud operation
+    app.get('/manualentry/employee',(request, response)=>{
+        pool.query('SELECT me.id,e.emp_code,me.item_count,me.code,me.id, me.canteen_type, me.item_id,me.category_id,c.category_name, e.emp_name,me.date,me.status FROM `manual_entry` me JOIN employee_master e ON e.emp_code = me.emp_code    JOIN category_master c ON c.id = me.category_id',(err,result)=>{
             if(err) throw err;
-            response.end(JSON.stringify(result));
+            response.send(result);
         });
-    }
-    console.log(empEntryDtata);
-       
- });
-
- app.put('/manualentry/employeeedit/:id',(request, response)=>{
-    var empEntryDtata  = request.body;
-    console.log(empEntryDtata);
-        pool.query("UPDATE manual_entry SET ? WHERE id",empEntryDtata,(err,result)=>{
+    });
+    app.get('/manualentry/manualentryemp/:empcode',(request, response)=>{
+        var emp_code = request.params.empcode;
+        //console.log(emp_code);
+        pool.query("SELECT em.emp_name, em.category_id,cm.category_name FROM employee_master as em INNER JOIN category_master cm ON em.category_id=cm.id where em.emp_code= ?",emp_code,(err,result)=>{
             if(err) throw err;
-            response.end(JSON.stringify(result));
+            response.send(result);
         });
-    console.log(empEntryDtata);
-       
- });
+    });
+    app.get('/manualentry/employee/item',(request, response)=>{
+        var postData = request.body;
+        pool.query("select item_id,item_name from item_master",postData,(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    });
+    app.get('/manualentry/employee/category',(request, response)=>{
+        var postData = request.body
+        pool.query("select id,category_name from category_master",postData,(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    });
+    app.post('/manualentry/manualentrynew',(request, response)=>{
+        var empEntryDtata = request.body;
+        var empEntyLength   = empEntryDtata.length;
+        for(i=0;i<empEntyLength;i++) {
+            pool.query("insert into  manual_entry SET ?",empEntryDtata[i],(err,result)=>{
+                if(err) throw err;
+                response.end(JSON.stringify(result));
+            });
+            console.log(empEntryDtata);
+        }  
+    });
 
- app.delete('/manualentry/employeedelete/:id',(request, response)=>{
-    var id = request.params.id;
-    pool.query("delete from manual_entry where id= ?",id,(err,result)=>{
+    app.put('/manualentry/employeeedit/:id',(request, response)=>{
+        var empEntryDtata  = request.body;
+        console.log(empEntryDtata);
+            pool.query("UPDATE manual_entry SET ? WHERE id",empEntryDtata,(err,result)=>{
+                if(err) throw err;
+                response.end(JSON.stringify(result));
+            });
+        console.log(empEntryDtata);
+        
+    });
+
+    app.delete('/manualentry/employeedelete/:id',(request, response)=>{
+        var id = request.params.id;
+        pool.query("delete from manual_entry where id= ?",id,(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    });
+
+
+    //Contractor crud operation
+    app.get('/manualentry/contractor/users',(request, response)=>{
+        pool.query('SELECT me.id,e.emp_code,me.item_count,me.code,me.id, me.canteen_type, me.item_id, c.category_name, e.emp_name,me.date,me.status FROM `guest_manual_entry` me JOIN employee_master e ON e.emp_code = me.emp_code    JOIN category_master c ON c.id = me.category_id',(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    });
+    //update
+    app.put('/manualentry/contractor/contedit/:id',(request, response)=>{
+        var contEntryDtata  = request.body;
+        console.log(contEntryDtata);
+            pool.query("UPDATE manual_entry SET ? WHERE id",contEntryDtata,(err,result)=>{
+                if(err) throw err;
+                response.end(JSON.stringify(result));
+            });
+    });
+    //delete
+    app.delete('/manualentry/contractor/contdelete/:id',(request, response)=>{
+        var id = request.params.id;
+        pool.query("delete from manual_entry where id= ?",id,(err,result)=>{
+            if(err) throw err;
+            response.send(result);
+        });
+    });
+
+
+//Meeting Request
+app.get('/manualentry/meetingreq/view',(request, response)=>{
+    pool.query('SELECT * from meeting_header',(err,result)=>{
         if(err) throw err;
         response.send(result);
     });
 });
-
-
-//Contractor crud operation
-app.get('/manualentry/contractor/users',(request, response)=>{
-    pool.query('SELECT me.id,e.emp_code,me.item_count,me.code,me.id, me.canteen_type, me.item_id, c.category_name, e.emp_name,me.date,me.status FROM `guest_manual_entry` me JOIN employee_master e ON e.emp_code = me.emp_code    JOIN category_master c ON c.id = me.category_id',(err,result)=>{
+app.get('/manualentry/meetingreq/itemview',(request, response)=>{
+    pool.query('SELECT * from item_master where item_id in (1,2,3,4,10,11,12,13,14)',(err,result)=>{
         if(err) throw err;
         response.send(result);
     });
-
 });
-
-
-
-
 
 
 
